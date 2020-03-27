2b248ab083e5fe3e1ce07665adcd0b069da0bd61
gjovic1 <gjovic1@etf.unsa.ba>
2020-03-27 11:26:21 +0100

    UI drugog zadatka napravljen, funkcija izbacivanja duplikata ispravno radi jer je testirano ručno.

--- src/ba/unsa/etf/rs/tut4/Controller.java
+++ src/ba/unsa/etf/rs/tut4/Controller.java
@@ -1,4 +1,30 @@
 package ba.unsa.etf.rs.tut4;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+import java.util.ArrayList;
+
 public class Controller {
+    public TextArea beforeIzbaciArea;
+    public TextArea afterIzbaciArea;
+    public Button btnIzbaci;
+
+    public void izbaciAction(ActionEvent actionEvent) {
+        String arts = beforeIzbaciArea.getText();
+        String[] artikli = arts.split("\n");
+        ArrayList<Artikal> smece = new ArrayList<>();
+
+        for(String el : artikli){
+            smece.add(new Artikal(el));
+        }
+        smece = Artikal.izbaciDuplikate(smece);
+
+        String res = "";
+        for(Artikal el : smece){
+            res += el + "\n";
+        }
+
+        afterIzbaciArea.setText(res);
+    }
 }
--- src/ba/unsa/etf/rs/tut4/Main.java
+++ src/ba/unsa/etf/rs/tut4/Main.java
@@ -1,7 +1,23 @@
 package ba.unsa.etf.rs.tut4;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/shop.fxml"));
+        primaryStage.setTitle("Kasa");
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.show();
+    }
     public static void main(String[] args) {
-        System.out.println("Sretan rad na tutorijalu 4!");
+        launch(args);
     }
 }
